c1ef227441bf3265f2097e4e6411bcd96a87e83e
pravtz <ederson.ufrgs@gmail.com>
2023-07-27 20:17:50 -0300

    start

---
+++ src/http/server.ts
@@ -0,0 +1,8 @@
+import app from './app'
+import { logger } from '../utils/logger'
+
+const PORT = process.env.PORT || 8081
+
+app.listen(PORT, () => {
+    logger.info(`🚀 Server started on port ${PORT}!`)
+})
